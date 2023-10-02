934b2e1c9380b9942a11f33554dbbb4b4bae3946
Ganesh Kumar <mrganeshofficial8@gmail.com>
2023-10-02 17:29:10 +0530

    id based search

--- nem111_backend/routes/Product.routes.js
+++ nem111_backend/routes/Product.routes.js
@@ -17,11 +17,19 @@ productRoutes.post("/add", async (req, res) => {
   }
 });
 
-productRoutes.get("/", async (req, res) => {
+productRoutes.get("/:id", async (req, res) => {
   let { city, type, page, limit, search } = req.query;
-
+  const { id } = req.params; 
+console.log(id)
   try {
-    if (city && type) {
+    if (id) { 
+      const data = await ProductModel.findById({_id:id}); 
+      if (data) {
+        res.status(200).json({ data: data });
+      } else {
+        res.status(404).json({ error: "Product not found" });
+      }
+    }else if (city && type) {
       const data = await ProductModel.find({
         $and: [{ city: city }, { type: type }],
       });
@@ -33,8 +41,8 @@ productRoutes.get("/", async (req, res) => {
       const data = await ProductModel.find({ type: type });
       res.status(200).json({ data: data });
     } else if (page) {
-      const page = parseInt(req.query.page) || 1; // Default to page 1 if page parameter is not provided
-      const limit = parseInt(req.query.limit) || 10; // Default to 10 items per page if limit parameter is not provided
+      const page = parseInt(req.query.page) || 1;
+      const limit = parseInt(req.query.limit) || 10;
 
       try {
         const totalProducts = await ProductModel.countDocuments();
@@ -56,13 +64,12 @@ productRoutes.get("/", async (req, res) => {
       try {
         let query = {};
         const searchTerm = search;
-        // If search term is provided, add it to the query
         if (searchTerm) {
           query = {
             $or: [
-              { city: { $regex: searchTerm, $options: "i" } }, // Case-insensitive search on title
-              { type: { $regex: searchTerm, $options: "i" } }, // Case-insensitive search on author
-              { location: { $regex: searchTerm, $options: "i" } }, // Case-insensitive search on genre
+              { city: { $regex: searchTerm, $options: "i" } },
+              { type: { $regex: searchTerm, $options: "i" } },
+              { location: { $regex: searchTerm, $options: "i" } },
             ],
           };
         }
@@ -70,10 +77,81 @@ productRoutes.get("/", async (req, res) => {
         const products = await ProductModel.find(query);
         res.json(products);
       } catch (err) {
-        res.status(500).json({ error: "Failed to fetch books" });
+        res.status(500).json({ error: "Failed to fetch products" });
       }
     } else {
-      const data = await ProductModel.find({});
+      const data = await ProductModel.find();
+      res.status(200).json({ data: data });
+    }
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
+
+productRoutes.get("/", async (req, res) => {
+  let { city, type, page, limit, search } = req.query;
+  const { id } = req.params; // Change req.params.id to req.query.id
+console.log(id)
+  try {
+    if (id) { // Check if id is provided
+      const data = await ProductModel.findById({_id:id}); // Use findById to search by id
+      if (data) {
+        res.status(200).json({ data: data });
+      } else {
+        res.status(404).json({ error: "Product not found" });
+      }
+    }else if (city && type) {
+      const data = await ProductModel.find({
+        $and: [{ city: city }, { type: type }],
+      });
+      res.status(200).json({ data: data });
+    } else if (city) {
+      const data = await ProductModel.find({ city: city });
+      res.status(200).json({ data: data });
+    } else if (type) {
+      const data = await ProductModel.find({ type: type });
+      res.status(200).json({ data: data });
+    } else if (page) {
+      const page = parseInt(req.query.page) || 1;
+      const limit = parseInt(req.query.limit) || 10;
+
+      try {
+        const totalProducts = await ProductModel.countDocuments();
+        const totalPages = Math.ceil(totalProducts / limit);
+
+        const product = await ProductModel.find()
+          .skip((page - 1) * limit)
+          .limit(limit);
+
+        res.json({
+          currentPage: page,
+          totalPages,
+          data: product,
+        });
+      } catch (err) {
+        res.status(500).json({ error: "Failed to fetch data" });
+      }
+    } else if (search) {
+      try {
+        let query = {};
+        const searchTerm = search;
+        if (searchTerm) {
+          query = {
+            $or: [
+              { city: { $regex: searchTerm, $options: "i" } },
+              { type: { $regex: searchTerm, $options: "i" } },
+              { location: { $regex: searchTerm, $options: "i" } },
+            ],
+          };
+        }
+
+        const products = await ProductModel.find(query);
+        res.json(products);
+      } catch (err) {
+        res.status(500).json({ error: "Failed to fetch products" });
+      }
+    } else {
+      const data = await ProductModel.find();
       res.status(200).json({ data: data });
     }
   } catch (err) {
